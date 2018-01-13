cd15fe3afb36a74b2a0ce59b51379451a9d5ce57
myf19 <myf1995@gmail.com>
2018-01-13 16:46:02 +0100

    First refactor for tests, removed initial declaration and globalized it.

--- src/test.java
+++ src/test.java
@@ -4,46 +4,45 @@ import org.junit.Test;
 
 
 public class test {
+    private main calculator;
+
+    @Before
+    public void init(){
+        calculator = new main();
+    }
 
     @Test
     public void emptyStringReturnsZero() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate(""), 0);
     }
 
     @Test
     public void singleValueIsReplied() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate("1"), 1);
     }
 
     @Test
     public void twoNumbersCommaDelimitedReturnSum() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate("1,2"),3);
     }
 
     @Test
     public void twoNumbersNewLineDelimitedReturnSum() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate("1\n2"),3);
     }
 
     @Test
     public void threeNumbersDelimitedBothWaysReturnSum() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate("1,2,3"), 6);
     }
 
     @Test(expected = Exception.class)
     public void negativeInputReturnsException() throws Exception {
-        main calculator = new main();
         calculator.calculate("-1");
     }
 
     @Test
     public void ignoresNumbersGreaterThanAThousand() throws Exception {
-        main calculator = new main();
         Assert.assertEquals(calculator.calculate("10,10,1001"), 20);
     }
 }
