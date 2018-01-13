6205a17d97de66f70d8afc9824a616f96d83cac3
myf19 <myf1995@gmail.com>
2018-01-13 17:04:13 +0100

    New methods created for refactoring purposes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,30 @@
 public class Calculator {
 
-    public int calculate(String s) throws Exception {
-        String[] numbers = s.split(",|\n");
-        if(s.isEmpty()){
+    public int calculate(String input) throws Exception {
+        String[] numbers = input.split(",|\n");
+        if(input.isEmpty()){
             return 0;
         }
-        if(s.length() == 1){
-            return Integer.parseInt(s);
+        if(input.length() == 1){
+            return Integer.parseInt(input);
         } else {
-            int sum = 0;
-            for (int i = 0; i < numbers.length; i++) {
-                if(Integer.parseInt(numbers[i]) < 0){
-                    throw new Exception("Negative input is not allowed");
-                }
-            }
-            for (int i = 0; i < numbers.length; i++) {
-                if(Integer.parseInt(numbers[i]) > 1000){
-                    continue;
-                }
-                sum += Integer.parseInt(numbers[i]);
-            }
-            return sum;
+           return getSum(numbers);
         }
+    }
+
+    private int getSum(String[] numbers) throws Exception {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if(Integer.parseInt(numbers[i]) < 0){
+                throw new Exception("Negative input is not allowed");
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if(Integer.parseInt(numbers[i]) > 1000){
+                continue;
+            }
+            sum += Integer.parseInt(numbers[i]);
+        }
+        return sum;
     }
 }
