fbb4ff9bcabb34e82f85e94423a3d4dc63b523da
myf19 <myf1995@gmail.com>
2018-01-13 16:40:29 +0100

    Initial code for String-Calculator, no refactor

--- src/main.java
+++ src/main.java
@@ -1,2 +1,26 @@
 public class main {
+
+    public int calculate(String s) throws Exception {
+        String[] numbers = s.split(",|\n");
+        if(s.isEmpty()){
+            return 0;
+        }
+        if(s.length() == 1){
+            return Integer.parseInt(s);
+        } else {
+            int sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                if(Integer.parseInt(numbers[i]) < 0){
+                    throw new Exception("Negative input is not allowed");
+                }
+            }
+            for (int i = 0; i < numbers.length; i++) {
+                if(Integer.parseInt(numbers[i]) > 1000){
+                    continue;
+                }
+                sum += Integer.parseInt(numbers[i]);
+            }
+            return sum;
+        }
+    }
 }
